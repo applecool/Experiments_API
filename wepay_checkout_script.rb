5d5b16ae586ecd7611a50e1bddd25975e01fd608
applecool <kmitharsha@gmail.com>
2015-03-03 03:46:36 -0600

    Added comments

--- wepay_checkout_script.rb
+++ wepay_checkout_script.rb
@@ -8,7 +8,7 @@ headers = {
 		   'Content-Type' =>'application/json', 
 		   'User-Agent'   => 'Checkout WePay Demo'
 		  }
-params  = { 
+params  = { #parameters required to process a checkout according to the wepay api
 		   :account_id => 463624231,
 		   :short_description => 'Lego Mindstorms',
 		   :type => 'TOYS',
@@ -16,7 +16,7 @@ params  = {
 		  }
 url = URI.parse('https://stage.wepayapi.com/v2/checkout/create') # URL of the call
 call =  Net::HTTP::Post.new(url.path, initheader = headers)
-call.add_field('Authorization: Bearer', 'STAGE_df1684a1c7b91f0de51b72e5890891b92d34e47fb3cb48d4dbd8d2a89fa253cc') 
+call.add_field('Authorization: Bearer', 'STAGE_df1684a1c7b91f0de51b72e5890891b92d34e47fb3cb48d4dbd8d2a89fa253cc') #Add token generated with your username
 call.body = params.to_json
 
 request = Net::HTTP.new(url.host, url.port)
