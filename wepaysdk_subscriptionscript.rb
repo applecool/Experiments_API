a70139e17723a8ddd1c97a0adec6096e702f3879
applecool <kmitharsha@gmail.com>
2015-03-03 04:34:00 -0600

    Added comments

--- wepaysdk_subscriptionscript.rb
+++ wepaysdk_subscriptionscript.rb
@@ -1,4 +1,4 @@
-# download the Ruby SDK at https://github.com/wepay/ruby-sdk
+# download the Ruby SDK at https://github.com/wepay/ruby-sdk or install the sdk by running gem install wepay in terminal
 require 'wepay'
 
 # credentials
@@ -9,9 +9,9 @@ access_token = 'STAGE_df1684a1c7b91f0de51b72e5890891b92d34e47fb3cb48d4dbd8d2a89f
 wepay = WePay.new(client_id, client_secret)
 response = wepay.call('/preapproval/create', access_token, {
 	:account_id => 461624321,
-	:period => 'monthly',
-	:end_time => '2015-12-15',
-	:amount => '10.99',
+	:period => 'monthly', #how often the funds will be collected
+	:end_time => '2015-12-15', #subscription ending date
+	:amount => '10.99', 
 	:short_description => 'Subscription to the fitness app.',
 	:auto_recur => true #this should be true for all subscriptions
 })
